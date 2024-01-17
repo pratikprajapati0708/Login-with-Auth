6bdf8a5e2f7df62be26264a5a3e3b91d95c15784
thecodedguyy <prajapratik1998@gmail.com>
2024-01-18 01:47:55 +0530

    first commit

---
+++ client/src/App.js
@@ -0,0 +1,42 @@
+import React from 'react'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import Username from './components/Username';
+import Register from './components/Register';
+import Recovery from './components/Recovery';
+import Reset from './components/Reset';
+import PageNotFound from './components/PageNotFound';
+
+import './App.css';
+
+function App() {
+  const router = createBrowserRouter([
+    {
+        path : '/',
+        element : <Username></Username>
+    },
+    {
+        path : '/register',
+        element : <Register></Register>
+    },
+    {
+        path : '/recovery',
+        element : <Recovery></Recovery>
+    },
+    {
+        path : '/reset',
+        element : <Reset></Reset>
+    },
+    {
+        path : '/404',
+        element : <PageNotFound></PageNotFound>
+    },
+])
+
+  return (
+    <main>
+        <RouterProvider router={router}></RouterProvider>
+    </main>
+  );
+}
+
+export default App;
---
+++ client/src/components/Username.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Username = () => {
+  return (
+    <div><h1>Username</h1></div>
+  )
+}
+
+export default Username
