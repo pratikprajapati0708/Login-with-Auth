c36172498c3661f62e58fd2cfd18096f6ddaff5f
thecodedguyy <prajapratik1998@gmail.com>
2024-01-21 21:45:56 +0530

    api resgiter

--- server/router/route.js
+++ server/router/route.js
@@ -1,22 +1,30 @@
-import { Router } from 'express';
-import * as controller from '../controllers/appControllers.js'
+import { Router } from "express";
 const router = Router();
 
-//POST method
-router.route('/register').post(controller.register);
-//router.route('/registerMail').post(); //send the email
-router.route('/authenticate').post((req,res)=> res.end()); //Authenticate user
-router.route('/login').post(controller.login); //login in app
-
-//GET Method
-router.route('/user/:username').get(controller.getUser); //user with username
-router.route('/generateOTP').get(controller.generateOTP); //generate random otp
-router.route('/verifyOTP').get(controller.verifyOTP); //verify otp
-router.route('/createResetSession').get(controller.createResetSession); //reset all the variables
-
-//PUT Method
-router.route('/updateuser').put(controller.updateUser); //update user profiles
-router.route('/resetPassword').put(controller.resetPassword); //use to reset password
+/** import all controllers */
+import * as controller from '../controllers/appControllers.js';
+// import { registerMail } from '../controllers/mailer.js'
+// import Auth, { localVariables } from '../middleware/auth.js';
 
 
-export default Router
+
+/** POST Methods */
+router.route('/register').post(controller.register); // register user
+// router.route('/registerMail').post(registerMail); // send the email
+router.route('/authenticate').post((req, res) => res.end()); // authenticate user
+router.route('/login').post(controller.login); // login in app
+
+/** GET Methods */
+router.route('/user/:username').get(controller.getUser) // user with username
+router.route('/generateOTP').get(controller.generateOTP) // generate random OTP
+router.route('/verifyOTP').get(controller.verifyOTP) // verify generated OTP
+router.route('/createResetSession').get(controller.createResetSession) // reset all the variables
+
+
+/** PUT Methods */
+router.route('/updateuser').put(controller.updateUser); // is use to update the user profile
+router.route('/resetPassword').put(controller.resetPassword); // use to reset password
+
+
+
+export default router;
--- server/server.js
+++ server/server.js
@@ -1,39 +1,38 @@
-//const express = require('express');
-//const cors = require('cors');
-//const morgon = require('morgan');
 import express from 'express';
 import cors from 'cors';
 import morgan from 'morgan';
-import ConnectToDB from '../server/database/connection.js'
-import route from '../server/router/route.js';
+import ConnectToDB from './database/connection.js';
+import router from './router/route.js';
+
 const app = express();
 
-//middleware
+/** middlewares */
 app.use(express.json());
 app.use(cors());
 app.use(morgan('tiny'));
-app.disable('x-powered-by'); //less hackers know about my stack
+app.disable('x-powered-by'); // less hackers know about our stack
 
 
 const port = 8080;
-//HTTP Get Request
+
+/** HTTP GET Request */
 app.get('/', (req, res) => {
-  res.status(201).json("Home Get Request");
-})
+    res.status(201).json("Home GET Request");
+});
 
-//api routes
-app.use('/api',route)
 
-//start server only we have valid connection
+/** api routes */
+app.use('/api', router)
+
+/** start server only when we have valid connection */
 ConnectToDB().then(() => {
-  try {
-    app.listen(port, () => {
-      console.log(`Server connect to ${port}`);
-    })
-  } catch (error) {
-    console.log('Cannot connect to the server');
-  }
-}).catch(error=>{
-  console.log('Invalid database connection');
+    try {
+        app.listen(port, () => {
+            console.log(`Server connected to http://localhost:${port}`);
+        })
+    } catch (error) {
+        console.log('Cannot connect to the server')
+    }
+}).catch(error => {
+    console.log("Invalid database connection...!");
 })
-
